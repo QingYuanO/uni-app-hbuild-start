9eb084edcbeeada498a8789af8834f4c7ab7d2b7
qingyuano <211720950@qq.com>
2026-01-31 19:46:29 +0800

    update

--- hooks/useI18nSync.ts
+++ hooks/useI18nSync.ts
@@ -1,28 +1,47 @@
-import { computed } from "vue";
-import Locale from "wot-design-uni/locale";
+import { computed, onBeforeMount } from "vue";
 import i18n from "@/i18n";
+import { Locale } from "wot-design-uni";
+
 
 export type SupportedLocalesType = "zh-CN" | "en-US";
+
+export const SUPPORTED_LOCALES_DATA = {
+  en: "en-US",
+  cn: "zh-CN",
+};
 export const SUPPORTED_LOCALES = [
   "zh-CN",
   "en-US",
 ];
 
-function setLocale(locale: string) {
+function setLocale(locale: string, isInit?: boolean) {
+  const user = useUserStore();
   if (!SUPPORTED_LOCALES.includes(locale)) {
     console.warn(`不支持的语言: ${locale}，将使用默认语言 zh-CN`);
     locale = "zh-CN";
   }
-  uni.setLocale(locale);
+
   i18n.global.locale.value = locale as SupportedLocalesType;
   uni.setStorageSync("currentLang", locale);
+
   Locale.use(locale);
+
+  if (!isInit) {
+    if (user.token) {
+      // uni.$api.common.languageUpdate({ language: locale as SupportedLocalesType }, { extraConfig: { showLoading: false } }).then((res) => {
+      //   uni.setLocale(locale);
+      // });
+    }
+    else {
+      uni.setLocale(locale);
+    }
+  }
   return locale;
 }
 
 function initLocale(defaultLocale: string) {
   const storedLocale = uni.getStorageSync("currentLang") || defaultLocale;
-  setLocale(storedLocale);
+  setLocale(storedLocale, true);
 }
 
 interface I18nSyncOptions {
@@ -35,7 +54,8 @@ interface I18nSyncOptions {
  * @param options 配置选项
  * @returns 国际化相关方法和状态
  */
-export function useI18nSync() {
+export function useI18nSync(options?: I18nSyncOptions) {
+  const { defaultLocale = "zh-CN" } = options || {};
   const currentLang = computed(() => i18n.global.locale.value);
 
   return {
